baf39476aa4d68fd9d4ddf4c33e7ffc2d461e650
Torham <torhamdev@gmail.com>
2023-03-04 12:30:24 +0330

    Add ForeignKey to user for flash card model

--- card/models.py
+++ card/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class FlashCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.TextField()
     answer = models.TextField()
 
